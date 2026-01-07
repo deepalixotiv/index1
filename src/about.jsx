29f3951c91157d04e2a7b82e9ff557b8fc4cab91
deepalixotiv <deepali.xotiv@gmail.com>
2026-01-07 10:02:46 +0530

    created-button

--- src/about.jsx
+++ src/about.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const about = () => {
   return (
+    <>
     <div>hello world</div>
+    <button className='bg-black text-white'>click me</button>
+    </>
+    
   )
 }
 
